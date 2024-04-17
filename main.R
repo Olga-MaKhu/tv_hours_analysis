3b2fc980e43e0eb72b75ddbb274d5067e7617dfc
Olga_MaKhu <105928605+Olga-MaKhu@users.noreply.github.com>
2024-04-17 22:45:44 +0200

    Update main.R_shorten names

--- main.R
+++ main.R
@@ -2,10 +2,10 @@ if(!require(pacman)) install.packages(pacman)
 pacman::p_load(tidyverse,
                here)
 
-tv_hr_table <- 
+tv_hr_tab <- 
   gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hrs = mean(tvhours, na.rm = T))
 
-write_csv(tv_hr_table, here("TV_hrs_by_marital.csv"))
+write_csv(tv_hr_tab, here("TV_hrs_by_marital.csv"))
